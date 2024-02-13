1f6d815e9d5e1efc4371096a198c73ff1fd2ddc0
bart2559b <152243939+bart2559b@users.noreply.github.com>
2024-02-13 11:14:13 +0700

    Add files via upload

---
+++ HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/controller/CustomerService.java
@@ -0,0 +1,47 @@
+package th.ac.ku.kps.eng.cpe.controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import th.ac.ku.kps.eng.cpe.dao.CustomerDAO;
+import th.ac.ku.kps.eng.cpe.entity.Customer;
+
+@Path("/services")
+public class CustomerService {
+	CustomerDAO cusDao = new CustomerDAO();
+
+	@GET
+	@Path("/customers")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Customer> getUsers() {
+		return cusDao.getAllCustomers();
+	}
+	/*
+	@GET
+	@Path("/customers/{param}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Customer getCustomerByname(@PathParam("param") String name) {
+		return cusDao.getCustomerByName(name);
+	}
+	 */
+	@POST
+	@Path("/customers/create")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createCustomer(Customer cus) throws IOException {
+		boolean i = cusDao.addCustomer(cus);
+		if (i == true)
+			return Response.status(201).entity(" create successfully").build();
+		else
+			return Response.status(201).entity(" create fail").build();
+	}
+	
+}
---
+++ HelloServlet/src/main/java/th/ac/ku/kps/eng/cpe/dao/CustomerDAO.java
@@ -0,0 +1,41 @@
+package th.ac.ku.kps.eng.cpe.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.TransactionException;
+
+import th.ac.ku.kps.eng.cpe.entity.Customer;
+import th.ac.ku.kps.eng.cpe.entity.Phonenumber;
+
+public class CustomerDAO {
+	public ArrayList<Customer> getAllCustomers() {
+		Session session = SessionUtil.getSession();
+		Query query = session.createQuery("from Customer");
+		ArrayList<Customer> customers = (ArrayList<Customer>) query.list();
+		session.close();
+		return customers;
+	}
+
+	public boolean addCustomer(Customer c) {
+		try {
+			Session session = SessionUtil.getSession();
+			Transaction tx = session.beginTransaction();
+			tx.begin();
+			session.save(c);
+			tx.commit();
+			session.close();
+		} catch (TransactionException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+
+}
